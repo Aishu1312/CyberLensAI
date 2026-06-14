4f18700387d6e957e009e47e53527a2371d9e7dc
Aishwarya <162090975+Aishu1312@users.noreply.github.com>
2026-06-15 00:00:48 +0530

    Update timeline.py

--- data/models/modules/timeline.py
+++ data/models/modules/timeline.py
@@ -1,7 +1,9 @@
 import plotly.express as px
+from modules.timeline import create_timeline
 
 def create_timeline(df):
-
+fig = create_timeline(df)
+st.plotly_chart(fig)
     fig = px.scatter(
         df,
         x="Date",
